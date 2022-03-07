9d0b81872285e8eed3c59e35f217553b20a58a84
Soobin09 <0921soobin@gmail.com>
2022-03-08 01:38:06 +0900

    ✨ console logger 추가
    winston으로 info, error, warn 등 일반 콘솔 로그 출력하고,
    morgan으로 api 요청 관련 로그를 콘솔에 출력한다.

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ import Sequelize from 'sequelize';
 import config from '../config/index.js';
 
 // logging => 쿼리문이 콘솔에 로그로 출력되는 것 비활성화
-const sequelize = new Sequelize(config.databaseURL, { logging: true });
+const sequelize = new Sequelize(config.databaseURL, { logging: false });
 
 export default sequelize;
