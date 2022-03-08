fdf78dbe6f020d9c71b35f36ddf28ce02d638138
sjhlko <76868151+sjhlko@users.noreply.github.com>
2022-03-09 01:05:13 +0900

    🐛 이메일,닉네임 중복처리 수정
    중복확인 버튼 눌러서 확인시 user api에서 확인함
    확인하지않고 회원가입 시도할때의 중복확인만 처리하는것으로 변경

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -9,44 +9,17 @@ const route = Router();
 
 export default app => {
   app.use('/auth', route);
+  //닉네임,이메일을 버튼을 눌러서 중복확인시 user api에서 확인
 
-  //회원가입시 이메일 중복 확인
-  route.get(
-    '/register/local/email-exists',
-    middlewares.isEmailExists,
-    wrapAsyncError(async (req, res) => {
-      return res.json({
-        isExists: false,
-      });
-    }),
-  );
-
-  //회원가입시 닉네임 중복 확인
-  route.get(
-    '/register/local/nickname-exists',
-    middlewares.isNicknameExists,
-    wrapAsyncError(async (req, res) => {
-      return res.json({
-        isExists: false,
-      });
-    }),
-  );
-
-  //로컬 회원가입
+  //로컬 회원가입(중복확인을 하지않고 회원가입 요청시)
   route.post(
     '/register/local',
     middlewares.isEmailExists,
     middlewares.isNicknameExists,
     wrapAsyncError(async (req, res) => {
       console.log(req.body);
-
       let account = null;
-      try {
-        account = await AuthServiceInstance.localRegister(req.body);
-      } catch (e) {
-        throw (500, e);
-      }
-
+      account = await AuthServiceInstance.localRegister(req.body);
       res.body = account;
       return res.json(res.body);
     }),
