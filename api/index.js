48077c6e837fc718b94021e7e6e5277d11491abb
LAPTOP-LGL711F3\yoona <yoona0427@naver.com>
2022-03-10 17:30:14 +0900

    :fire: insert.js, search.js 삭제
    posts.js로 합쳐진 파일 삭제

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-import insert from './routes/insert.js';
 import posts from './routes/posts.js';
-import search from './routes/search.js';
 import user from './routes/user.js';
 import auth from './routes/auth.js';
 
@@ -9,8 +7,6 @@ export default () => {
   const app = Router();
   user(app);
   auth(app);
-  search(app);
   posts(app);
-  insert(app);
   return app;
 };
