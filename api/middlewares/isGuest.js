d1ccc0da4a929a840290375ffeb733e8cb5dd7b6
Soobin09 <0921soobin@gmail.com>
2022-04-13 17:27:23 +0900

    🐛 게스트 확인 미들웨어 수정

--- api/middlewares/isGuest.js
+++ api/middlewares/isGuest.js
@@ -1,7 +1,12 @@
 import { wrapAsyncError } from '../../library/index.js';
 
 const isGuest = wrapAsyncError(async (req, res, next) => {
-  if (req.headers.authorization) return next();
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer') &&
+    req.cookies['refresh']
+  )
+    return next();
   next('route');
 });
 
